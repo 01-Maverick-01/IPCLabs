1a57c1e381b4d6e38e4e8674cab4121de35f3d9d
Ishan <deep24@owens-rw01.ten.osc.edu>
2020-03-20 04:43:42 -0400

    updated diff.sh and getTime.sh

--- diff.sh
+++ diff.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
-
-things="serial 2-2 4-4 8-8 16-16 32-32"
+path1=./lab1_output
+path2=./lab3_output
+things="2-2 4-4 8-8 16-16 32-32 64-64"
+mkdir diff
 for n in $(echo $things); do
-    diff ./1-1/x1/current.out ./$n/x1/current.out > ./diff/1-$n-x1.txt
-    cat ./diff/1-$n-x1.txt | wc -l
+    diff $path1/x1/current.out $path2/$n/x1/current.out > ./diff/serial-$n-x1.txt
+    cat ./diff/serial-$n-x1.txt | wc -l
     
-    diff ./1-1/00100/current.out ./$n/00100/current.out > ./diff/1-$n-00100.txt
-    cat ./diff/1-$n-00100.txt | wc -l
+    diff $path1/00100/current.out $path2/$n/00100/current.out > ./diff/serial-$n-00100.txt
+    cat ./diff/serial-$n-00100.txt | wc -l
 
-    diff ./1-1/01000/current.out ./$n/01000/current.out > ./diff/1-$n-01000.txt
-    cat ./diff/1-$n-01000.txt | wc -l
+    diff $path1/01000/current.out $path2/$n/01000/current.out > ./diff/serial-$n-01000.txt
+    cat ./diff/serial-$n-01000.txt | wc -l
 
-    diff ./1-1/05000/current.out ./$n/05000/current.out > ./diff/1-$n-05000.txt
-    cat ./diff/1-$n-05000.txt | wc -l
+    diff $path1/05000/current.out $path2/$n/05000/current.out > ./diff/serial-$n-05000.txt
+    cat ./diff/serial-$n-05000.txt | wc -l
 
-    diff ./1-1/10000/current.out ./$n/10000/current.out > ./diff/1-$n-10000.txt
-    cat ./diff/1-$n-10000.txt | wc -l
+    diff $path1/10000/current.out $path2/$n/10000/current.out > ./diff/serial-$n-10000.txt
+    cat ./diff/serial-$n-10000.txt | wc -l
 done
 
 
--- getTime.sh
+++ getTime.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
-things="serial 1-1 2-2 4-4 8-8 16-16 32-32"
+path=./lab3_output
+things="2-2 4-4 8-8 16-16 32-32 64-64"
 for n in $(echo $things); do
     echo $n-x1
-    cat ./$n/x1/prod_cons_single_thread.e* | grep "real"
-    cat ./$n/x1/prod_cons_single_thread.e* | grep "user"
+    cat $path/$n/x1/prod_cons_single_thread.e* | grep "real"
+    cat $path/$n/x1/prod_cons_single_thread.e* | grep "user"
     
     echo $n-100
-    cat ./$n/00100/prod_cons_single_thread.e* | grep "real"
-    cat ./$n/00100/prod_cons_single_thread.e* | grep "user"
+    cat $path/$n/00100/prod_cons_single_thread.e* | grep "real"
+    cat $path/$n/00100/prod_cons_single_thread.e* | grep "user"
 
     echo $n-1000
-    cat ./$n/01000/prod_cons_single_thread.e* | grep "real"
-    cat ./$n/01000/prod_cons_single_thread.e* | grep "user"
+    cat $path/$n/01000/prod_cons_single_thread.e* | grep "real"
+    cat $path/$n/01000/prod_cons_single_thread.e* | grep "user"
 
     echo $n-5000
-    cat ./$n/05000/prod_cons_single_thread.e* | grep "real"
-    cat ./$n/05000/prod_cons_single_thread.e* | grep "user"
+    cat $path/$n/05000/prod_cons_single_thread.e* | grep "real"
+    cat $path/$n/05000/prod_cons_single_thread.e* | grep "user"
 
     echo $n-10000
-    cat ./$n/10000/prod_cons_single_thread.e* | grep "real"
-    cat ./$n/10000/prod_cons_single_thread.e* | grep "user"
+    cat $path/$n/10000/prod_cons_single_thread.e* | grep "real"
+    cat $path/$n/10000/prod_cons_single_thread.e* | grep "user"
 done
 
 
